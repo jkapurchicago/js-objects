938432003dc4c51945686851d3ab4d54fcd3d06b
Jatin Kapur <jkapur.work@gmail.com>
2019-09-22 20:42:47 -0500

    first commit

---
+++ object-basics.js
@@ -0,0 +1,18 @@
+//Objects 
+
+var dog = { 
+    name: "Bubba",
+    breed: "lab"
+}
+
+
+//Working with arrays 
+var dogs = ["Rusty", "Lucky", "Bubba"]; 
+
+dogs[1]; 
+console.log(dogs[1]); 
+
+dogs.push("wyatt"); 
+
+
+console.log(dogs); 
